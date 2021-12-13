05a9ed534ac554419010f4b6e9a2b4d4c3eccf8f
raimonvelasco <raimonvelasco6@gmail.com>
2021-12-13 17:32:09 +0100

    Login en proceso.

--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/RegisterActivity.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/RegisterActivity.kt
@@ -2,6 +2,7 @@ package cat.smartcoding.mendez.freedating.ui.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cat.smartcoding.mendez.freedating.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
